8d5a770a6ceeba73764b4c6f5eb9db169a6abe1c
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-10 17:03:03 +0100

    Add production settings.

---
+++ aluapp/aluapp/aluapp/settings_live.py
@@ -0,0 +1,11 @@
+from aluapp.settings import *
+
+DEBUG = TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['deone.webfactional.com']
+
+STATIC_URL = "http://deone.webfactional.com/static/"
+
+MEDIA_ROOT = "/home/deone/webapps/alu_media"
+
+MEDIA_URL = "/media/"
