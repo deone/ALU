2028cd1c20c482c5c21f8cd460375357898f45f4
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-10 21:47:24 +0100

    Display sucess message.

--- aluapp/aluapp/app/views.py
+++ aluapp/aluapp/app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .forms import *
-
-import unicodecsv
 
 def home(request):
     if request.method == 'POST':
@@ -16,11 +15,13 @@ def home(request):
 
     return render(request, 'home.html', {'form': form})
 
+@login_required
 def import_list(request):
     if request.method == 'POST':
         form = ImportForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Students imported successfully.')
     else:
         form = ImportForm()
 
