6ebe7b9217500fd9f10857197c85efeb57f48411
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-10 21:06:36 +0100

    Student model plus migration.

--- aluapp/aluapp/app/models.py
+++ aluapp/aluapp/app/models.py
@@ -15,3 +15,12 @@ class UserType(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Student(models.Model):
+    index_no = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name, self.last_name
