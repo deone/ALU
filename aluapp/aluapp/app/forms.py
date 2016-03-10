233f790cc12a2ad2976ae9f454f94a943037a9b8
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-10 16:35:20 +0100

    Save user_type.

--- aluapp/aluapp/app/forms.py
+++ aluapp/aluapp/app/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
 
 from .models import UserType
 
 class UserTypeForm(forms.Form):
-    user_types = forms.ChoiceField(choices=UserType.TYPE_CHOICES, widget=forms.RadioSelect)
+    user_type = forms.ChoiceField(choices=UserType.TYPE_CHOICES, widget=forms.RadioSelect)
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super(UserTypeForm, self).__init__(*args, **kwargs)
+
+    def save(self):
+        user = User.objects.get(username__iexact=self.user.username)
+        user_type = UserType.objects.create(user=user, user_type=self.cleaned_data['user_type'])
+        user_type.save()
--- aluapp/aluapp/app/views.py
+++ aluapp/aluapp/app/views.py
@@ -5,7 +5,13 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserTypeForm
 
 def home(request):
-    form = UserTypeForm()
+    if request.method == 'POST':
+        form = UserTypeForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserTypeForm(user=request.user)
+
     return render(request, 'home.html', {'form': form})
 
 def logout(request):
