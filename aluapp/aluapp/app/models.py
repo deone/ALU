ec5f21491c59731390865d27390c62b7000f1ad9
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-10 15:20:22 +0100

    UserType model.

--- aluapp/aluapp/app/models.py
+++ aluapp/aluapp/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserType(models.Model):
+    TEACHER = 'TCH'
+    STUDENT = 'STD'
+
+    TYPE_CHOICES = (
+        ('', 'Select Type'),
+        (TEACHER, 'Teacher'),
+        (STUDENT, 'Student'),
+    )
+    
+    user = models.OneToOneField(User)
+    user_type = models.CharField(max_length=3, choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return self.user.username
