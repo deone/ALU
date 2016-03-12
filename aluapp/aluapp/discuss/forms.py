72ddd4b0a80957da8e94780c0ee7c6e451dce5c3
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-12 15:40:22 +0100

    Display comment field on template.

--- aluapp/aluapp/discuss/forms.py
+++ aluapp/aluapp/discuss/forms.py
@@ -14,9 +14,10 @@ def get_categories():
 class NewTopicForm(forms.ModelForm):
     class Meta:
         model = Topic
-        fields = ['title', 'category']
+        fields = ['title', 'category', 'comment']
 
     def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
         super(NewTopicForm, self).__init__(*args, **kwargs)
         self.fields['title'].widget = forms.TextInput(attrs={'class': 'form-control'})
         self.fields['category'].widget = forms.Select(attrs={'class': 'form-control'})
