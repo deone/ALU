e5086eab0f200b7350d2e96d3a6350409e687554
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-12 12:24:27 +0100

    Discuss models.

--- aluapp/aluapp/discuss/models.py
+++ aluapp/aluapp/discuss/models.py
@@ -1,5 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Common(models.Model):
+    slug = models.SlugField(unique=True, editable=False)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        abstract = True
+    
+
+class Category(Common):
+    name = models.CharField(max_length=50)
+
+class Topic(Common):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category)
