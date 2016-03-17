c0bd5a86aaf3c4f6dd0a04cd31de5ad34fc1f818
Dayo Osikoya <alwaysdeone@gmail.com>
2016-03-17 22:19:37 +0100

    Display fields based on Document.upload_quantity.

--- aluapp/aluapp/app/models.py
+++ aluapp/aluapp/app/models.py
@@ -93,7 +93,6 @@ class Document(models.Model):
     user = models.ForeignKey(User)
     document_type = models.ForeignKey(DocumentType)
     document_request = models.ForeignKey(DocumentRequest)
-    # document = models.FileField(upload_to='documents/%d-%m-%Y')
     document = models.FileField(upload_to=get_upload_path)
     date_submitted = models.DateTimeField(default=timezone.now, editable=False)
 
